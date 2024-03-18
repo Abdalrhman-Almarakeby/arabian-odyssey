e5adcf4489125fe242ca2d8e88cad19f42f4c13e
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-03-18 20:41:39 +0200

    Add route for Error page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "@/components/Layout";
 import Home from "@/pages/Home";
+import Error from "@/pages/Error";
 import "./CSS/output.css";
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
       </Route>
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
