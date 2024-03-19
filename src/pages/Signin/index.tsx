98243feaa4f7680f62aa0032a80891559ddc8a1b
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-03-19 17:19:02 +0200

    Build Signup page.

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,3 +1,77 @@
-export default function Signin() {
-  return <div>Signin</div>;
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Signup() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+  }
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>): void {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  return (
+    <section className="flex flex-col items-center pt-6">
+      <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0">
+        <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
+          <h1 className="text-xl font-bold leading-tight tracking-tight text-black md:text-2xl">
+            Welcome agin!
+          </h1>
+          <form className="space-y-4 md:space-y-6" method="POST" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email" className="mb-2 block text-sm font-medium text-black">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-black focus:border-primary focus:ring-primary sm:text-sm"
+                placeholder="John_doe@example.com"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="mb-2 block text-sm font-medium text-black">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={formData.password}
+                onChange={handleChange}
+                placeholder="••••••••"
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-black focus:border-primary focus:ring-primary sm:text-sm"
+                required
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full rounded-lg bg-primary/90 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary/90 focus:outline-none focus:ring-4 focus:ring-black"
+            >
+              Sing in
+            </button>
+            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+              Don't have an account?{" "}
+              <Link className="font-medium text-primary hover:underline" to="/signup">
+                Sing up here
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
