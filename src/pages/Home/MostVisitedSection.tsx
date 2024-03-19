f6d921bd9aa613fb725f61ccaa4e2a7b8ee17cc8
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-03-19 15:36:57 +0200

    Add the Most Visited section.

---
+++ src/pages/Home/MostVisitedSection.tsx
@@ -0,0 +1,16 @@
+// import SectionHeading from "@/components/SectionHeading";
+//
+// export default function MostVisitedSection() {
+//   return (
+//     <section className="container flex flex-col items-center justify-center gap-8 px-4 lg:gap-12">
+//       <SectionHeading>Most Visited</SectionHeading>
+//
+//       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+//         <div className="flex gap-2.5">
+//           <div className="size-[70px] rounded-full bg-gray-500"></div>
+//           <div className="text-xl font-bold"></div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import HotAttractionsSection from "./HotAttractionsSection";
 import LandingSection from "./LandingSection";
 import VisitArabiaSection from "./VisitArabiaSection";
 import TopDestinationsSection from "./TopDestinationsSection";
+// import MostVisitedSection from "./MostVisitedSection";
 import CategoriesSection from "./CategoriesSection";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <VisitArabiaSection />
       <HotAttractionsSection />
       <TopDestinationsSection />
+      {/* <MostVisitedSection /> */}
       <CategoriesSection />
     </main>
   );
