12b0db5027c14dd996fba81ab4abd4a8b378406b
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-03-21 21:40:28 +0200

    Remove unused utils functions.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
-}
-
-export function formatCurrency(number: number): string {
-  return number.toLocaleString("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  });
 }
