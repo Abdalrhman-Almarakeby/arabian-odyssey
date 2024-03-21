be4ac78b9411d872c182a841b6b8148df3e6fba0
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-03-21 21:59:31 +0200

    Change a tags to Link tags in footer.

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -7,28 +7,28 @@ export default function Footer() {
     <footer className="m-4 rounded-lg bg-white shadow dark:bg-gray-800">
       <div className="mx-auto w-full max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
-          © {year} <HashLink to="/#" className="hover:underline"></HashLink>. All Rights Reserved.
+          © {year} <HashLink to="/#" className="hover:underline">Arabian Odyssey</HashLink>. All Rights Reserved.
         </span>
         <ul className="mt-3 flex flex-wrap items-center text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
-            <a href="#" className="me-4 hover:underline md:me-6">
+            <HashLink to="/#" className="me-4 hover:underline md:me-6">
               About
-            </a>
+            </HashLink>
           </li>
           <li>
-            <a href="#" className="me-4 hover:underline md:me-6">
+            <HashLink to="/#" className="me-4 hover:underline md:me-6">
               Privacy Policy
-            </a>
+            </HashLink>
           </li>
           <li>
-            <a href="#" className="me-4 hover:underline md:me-6">
+            <HashLink to="/#" className="me-4 hover:underline md:me-6">
               Licensing
-            </a>
+            </HashLink>
           </li>
           <li>
-            <a href="#" className="hover:underline">
+            <HashLink to="/#" className="hover:underline">
               Contact
-            </a>
+            </HashLink>
           </li>
         </ul>
       </div>
