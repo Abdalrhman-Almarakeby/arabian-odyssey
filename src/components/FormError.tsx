023659313c27df1fb544ed6278c268065606b497
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-03-22 12:54:11 +0200

    Update the Form Error component.

--- src/components/FormError.tsx
+++ src/components/FormError.tsx
@@ -4,7 +4,7 @@ type FormErrorProps = {
 
 export function FormError({ children }: FormErrorProps) {
   return (
-    <p className="rounded-lg border border-red-600 bg-red-200 px-2 py-2 text-sm font-semibold text-red-600 md:text-base lg:text-lg">
+    <p className="rounded-lg border border-red-600 bg-red-200 px-2 py-2 text-sm font-semibold  text-red-600 md:text-base xl:text-lg">
       {children}
     </p>
   );
