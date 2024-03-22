f6adeb476b9c7341e51a92d88fa7ac1f26cdd072
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-03-22 21:23:35 +0200

    Fix spelling.

--- src/components/SliderContent.tsx
+++ src/components/SliderContent.tsx
@@ -8,26 +8,26 @@ interface SliderContentProps {
 
 function SliderContent({ img, location }: SliderContentProps) {
   return (
-    <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white relative">
-      <div className="absolute z-10 right-0 top-0 h-full w-full flex flex-col items-center justify-center py-2 px-16">
-        <h1 className="text-white text-3xl sm:text-4xl font-blod mb-8 text-center">
+    <div className="relative flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+      <div className="absolute right-0 top-0 z-10 flex h-full w-full flex-col items-center justify-center px-16 py-2">
+        <h1 className="mb-8 text-center text-3xl font-bold text-white sm:text-4xl">
           Arabia: Beyond the Desert
         </h1>
-        <p className="text-center text-white text-sm sm:text-base font-light mb-4">
-          Embark on a journey to explore the vibrant tapestry of the Arab world,
-          where each step unveils treasures beyond the desert sands
+        <p className="mb-4 text-center text-sm font-light text-white sm:text-base">
+          Embark on a journey to explore the vibrant tapestry of the Arab world, where each step
+          unveils treasures beyond the desert sands
         </p>
-        <button className="bg-primary text-white font-bold text-base sm:text-xl rounded-[100px] px-4 py-2">
+        <button className="rounded-[100px] bg-primary px-4 py-2 text-base font-bold text-white sm:text-xl">
           Explore now
         </button>
       </div>
-      <div className="absolute bottom-10 z-10 right-0 mx-6 text-white">
+      <div className="absolute bottom-10 right-0 z-10 mx-6 text-white">
         <FontAwesomeIcon icon={faLocationDot} className="mr-2 text-xl" />
         <p className="inline-block">{location}</p>
       </div>
-      <figure className="top-0 right-0 w-full h-full">
-        <div className="bg-black w-full h-full opacity-20 absolute top-0 left-0"></div>
-        <img className="w-full h-full object-cover" src={img} alt="" />
+      <figure className="right-0 top-0 h-full w-full">
+        <div className="absolute left-0 top-0 h-full w-full bg-black opacity-20"></div>
+        <img className="h-full w-full object-cover" src={img} alt="" />
       </figure>
     </div>
   );
