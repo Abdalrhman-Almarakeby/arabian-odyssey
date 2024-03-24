be9e60bc9b63131cb113fc7ade9c1af6ae65ddf0
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-03-24 02:33:03 +0200

    Fix bug with routing.

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,20 +1,9 @@
-import { useEffect } from "react";
-import { useLocation, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { Loading } from "@/components/Loading";
 
 export function Layout({ loading }: { loading: boolean }) {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    document.documentElement.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "instant",
-    });
-  }, [pathname]);
-
   return (
     <div className="relative flex min-h-svh flex-col">
       {loading && <Loading />}
