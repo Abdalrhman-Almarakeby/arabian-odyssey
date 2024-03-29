4e8929cdc59242f914297bc1fdcc9d7572098885
HoussemDegachi <113510665+HoussemDegachi@users.noreply.github.com>
2024-03-30 00:45:25 +0200

    Update Map.tsx

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,7 @@
-import { LatLngExpression } from "leaflet";
+import { LatLngExpression, Icon } from "leaflet";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import MarkerSVG from "@/assets/icons/location-dot.svg";
 
 type MapProps = {
   cords: LatLngExpression;
@@ -8,13 +9,17 @@ type MapProps = {
 };
 
 export function Map({ cords, location }: MapProps) {
+  const customIcon = new Icon({
+    iconUrl: MarkerSVG,
+    iconSize: [38, 38],
+  });
   return (
     <MapContainer center={cords} zoom={13} className="h-full w-full">
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; arabian-odyssey"
       />
-      <Marker position={cords}>
+      <Marker position={cords} icon={customIcon}>
         <Popup>
           <p className="font-bold">{location}</p>
         </Popup>
